aeb0ea649e8d2401c33b3a399bc39a41cea7f4df
rolemberg <vinnie.nsts@gmail.com>
2024-03-12 01:30:41 -0300

    cart page

--- frontend/src/Apps/Cart.tsx
+++ frontend/src/Apps/Cart.tsx
@@ -1,66 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Generics/Header/Header';
 import ItensCart from '../components/Cart/itensCart/ItensCart';
 import Footer from '../components/Generics/Footer/Foot';
 
 export default function Cart() {
-   // const [ val, setVal ] = useState('0');
-
-   // useEffect(() => {
-   //    function getVals() {
-   //       const val = localStorage.getItem('val');
-
-   //       if(val) return setVal(val);
-   //    };
-   //    getVals();
-   // });
-
-   // function handleVal(e: any) {
-   //    const input = e.target;
-      
-   //    setVal(input.value);
-
-   //    localStorage.setItem('val', input.value);
-   //    return;
-   // };
-
    return(
       <div className=' overflow-x-hidden'>
          <section className='absolute top-0 left-0'>
             <Header />
          </section>
 
-         <section className='flex justify-center mt-32 w-screen min-h-screen'>
-            <section className=' w-9/12 h-fit border-2 border-black'>
-               <h1 className='p-1 text-3xl'>Carrinho</h1>
+         <section className=' w-screen flex items-center justify-center mt-36'>
+            <img className="shadow-lg shadow-slate-800 rounded-xl" src="/assets/arts/later.png" />
+         </section>
+
+         <section className='flex justify-center mt-32 w-screen min-h-fit'>
+
+            <section className='w-11/12 h-fit border-t-4 border-red-900 rounded-3xl'>
+
+               <section className='flex justify-evenly items-center p-4'>
+                  <h1 className='p-1 text-2xl font-semibold uppercase text-slate-900'>
+                     Carrinho
+                  </h1>
+
+                  <button type="button" className='text-sm text-red-500 hover:text-red-700 hover:underline'>
+                     Limpar carrinho
+                  </button>
+               </section>
 
                <ItensCart
                   nome={'SEDA BEM BOLADO BROWN KING SIZE SABOTAGE'}
                   preco={'R$4,50'} 
                />
 
-               {/* <section className='flex flex-col justify-evenly border-t border-b border-black md:flex-row'>
-                  <section className='p-1'>
-                     SEDA BEM BOLADO BROWN KING SIZE SABOTAGE   
-                  </section>
+               <section className='flex justify-evenly w-full h-fit p-4 border-t border-red-900'>
+                  <button 
+                     type="button" 
+                     className='m-1 px-3 py-0.5 text-sm uppercase bg-green-600 text-slate-900'
+                  >
+                     Adicionar mais itens
+                  </button>
 
-                  <section className='p-1'>
-                     <input 
-                        type="number" 
-                        value={val} 
-                        onChange={handleVal} 
-                        className='mx-3 w-10 border border-black' 
-                     />
-                  </section>
-
-                  <section className='p-1'>
-                     R$4,50
-                  </section>
-               </section> */}
+                  <button 
+                     type="button" 
+                     className='m-1 px-3 py-0.5 text-sm uppercase bg-green-700 text-slate-900'
+                  >
+                     Continuar compra
+                  </button>
+               </section>
             </section>
+
          </section>
 
          <Footer />
+
       </div>
    );
 };
--- frontend/src/components/Cart/itensCart/ItensCart.tsx
+++ frontend/src/components/Cart/itensCart/ItensCart.tsx
@@ -1,44 +1,70 @@
 import React, { useEffect, useState } from "react";
+import { FaTrash } from "react-icons/fa6";
 
 export default function ItensCart({ nome, preco }) {
-   const [ val, setVal ] = useState('0');
+   const [ quantity, setQauntity ] = useState('0');
 
    useEffect(() => {
       function getVals() {
-         const val = localStorage.getItem('quantidade');
+         const val = localStorage.getItem(nome);
 
-         if(val) return setVal(val);
+         const quantity = (val?.split('.')[0]);
+
+         return (val ? setQauntity(String(quantity)) : 0);
       };
       getVals();
    });
 
    function handleVal(e: any) {
-      const valor = e.target.value; 
+      const valor = e.target.value;
 
-      setVal(valor);
+      const data = `${valor}.${preco}`;
 
-      return localStorage.setItem('quantidade', valor);
+      setQauntity(valor);
+
+      return localStorage.setItem(nome, data);
    };
 
    return(
       <>
-         <section className='flex flex-col justify-evenly py-10 border-t-2 border-black md:flex-row'>
-            <section className='p-1'>
-               {nome}
-            </section>
+         <section className='flex flex-col justify-between py-10 border-t border-red-900 md:flex-row'>
 
-            <section className='p-1'>
-               <input 
-                  type="number" 
-                  value={val} 
-                  onChange={handleVal} 
-                  className='mx-3 w-10 border border-black' 
-               />
-            </section>
+            <table className='flex justify-center w-full'>
 
-            <section className='p-1'>
-               {preco}
-            </section>
+               <tr className="flex items-center justify-between w-screen md:w-11/12">
+                  <td className="md:w-5/12">
+                     <section className='p-1 ml-5 text-sm'>
+                        {nome}
+                     </section>
+                  </td>
+
+                  <td>
+                     <section className='p-1'>
+                        <input 
+                           type="number" 
+                           value={quantity} 
+                           onChange={handleVal} 
+                           className='mx-3 w-10 border border-black' 
+                        />
+                     </section>
+                  </td>
+
+                  <td>
+                  <section className='p-1'>
+                     {preco}
+                  </section>
+                  </td>
+
+                  <td>
+                     <section className="p-1 mr-1 md:mr-7 lg:mr-6 xl:mr-0">
+                        <FaTrash
+                           className=" text-rose-400 hover:text-red-600 hover:cursor-pointer"
+                        />
+                     </section>
+                  </td>
+               </tr>
+            </table>
+            
          </section>
       </>
    );
